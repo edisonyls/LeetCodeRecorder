f934829247024dcb4943df8c0573a39fbd901571
Edison Yang <edisonyls@hotmail.com>
2024-03-01 00:30:32 +1100

    feat: adding button and search field

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axiosInstance from "../config/axiosConfig";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
+import SearchIcon from "@mui/icons-material/Search";
 import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 import {
   Container,
@@ -11,6 +12,10 @@ import {
   TableHead,
   TableRow,
   Paper,
+  Box,
+  Button,
+  TextField,
+  InputAdornment,
 } from "@mui/material";
 
 const Dashboard = () => {
@@ -38,6 +43,62 @@ const Dashboard = () => {
 
   return (
     <Container>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          mb: 2,
+        }}
+      >
+        <Button
+          variant="outlined"
+          color="inherit"
+          sx={{
+            minWidth: "120px",
+            height: "40px",
+            borderRadius: "20px",
+            borderColor: "black",
+            color: "black",
+            "&:hover": {
+              borderColor: "black",
+              backgroundColor: "rgba(1, 1, 1, 1)",
+              color: "white",
+            },
+          }}
+        >
+          + New
+        </Button>
+        <TextField
+          label="Search..."
+          variant="outlined"
+          size="small"
+          sx={{
+            width: "240px",
+            "& .MuiOutlinedInput-root": {
+              // set border color black
+              "& fieldset": {
+                borderColor: "black",
+              },
+              // set border color black when hover
+              "&:hover fieldset": {
+                borderColor: "black",
+              },
+              // Set the border color when the input is focused
+              "&.Mui-focused fieldset": {
+                borderColor: "black",
+              },
+            },
+          }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <SearchIcon />
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Box>
       <TableContainer component={Paper}>
         <Table aria-label="simple table">
           <TableHead
