f41803067e352b229f77d7ef7e01e89e14b8f9ee
edisonyls <edisonyls@hotmail.com>
2025-04-02 14:14:57 +1100

    fix: fixing backend stats return

--- ylslc/src/main/java/com/yls/ylslc/question/QuestionRepository.java
+++ ylslc/src/main/java/com/yls/ylslc/question/QuestionRepository.java
@@ -18,7 +18,8 @@ public interface QuestionRepository extends JpaRepository<QuestionEntity, UUID> 
     Page<QuestionEntity> findByUser(UserEntity currentUser, Pageable pageable);
 
     @Query("SELECT q FROM QuestionEntity q WHERE q.user = :user AND (LOWER(q.title) LIKE LOWER(CONCAT('%', :searchQuery, '%')) OR CAST(q.number AS string) LIKE %:searchQuery%)")
-    Page<QuestionEntity> searchByTitleOrNumber(@Param("user") UserEntity user, @Param("searchQuery") String searchQuery, Pageable pageable);
+    Page<QuestionEntity> searchByTitleOrNumber(@Param("user") UserEntity user, @Param("searchQuery") String searchQuery,
+            Pageable pageable);
 
     @Query("SELECT q FROM QuestionEntity q WHERE q.id = :id AND q.user.username = :username")
     Optional<QuestionEntity> findByIdAndUsername(@Param("id") UUID id, @Param("username") String username);
@@ -29,7 +30,12 @@ public interface QuestionRepository extends JpaRepository<QuestionEntity, UUID> 
     @Query("SELECT q.difficulty AS difficulty, COUNT(q) AS count FROM QuestionEntity q WHERE q.user.id = :userId GROUP BY q.difficulty")
     List<Map<String, Object>> findDifficultyDistributionByUserId(@Param("userId") UUID userId);
 
-    @Query("SELECT q.success AS success, COUNT(q) AS count FROM QuestionEntity q WHERE q.user.id = :userId GROUP BY q.success")
+    @Query(value = """
+              SELECT s.success, COALESCE(COUNT(q.id), 0) AS count
+              FROM (SELECT TRUE AS success UNION SELECT FALSE) s
+              LEFT JOIN question q ON q.success = s.success AND q.user_id = :userId
+              GROUP BY s.success
+            """, nativeQuery = true)
     List<Map<String, Object>> findSuccessDistributionByUserId(@Param("userId") UUID userId);
 
     @Query("SELECT DATE(q.createdAt) AS createdAtDate, COUNT(q) AS count FROM QuestionEntity q WHERE q.user.id = :userId GROUP BY DATE(q.createdAt) ORDER BY DATE(q.createdAt) DESC")
@@ -42,4 +48,3 @@ public interface QuestionRepository extends JpaRepository<QuestionEntity, UUID> 
     List<Map<String, Object>> findAverageTimeOfCompletionByDifficulty(@Param("userId") UUID userId);
 
 }
-
